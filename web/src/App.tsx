4d30f3776deeb8cb0963d09eaf5340f71c694de5
Gabriel Manika <gmkoeb@gmail.com>
2024-10-24 10:54:57 -0300

    Initial front-end setup with vite

---
+++ web/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1 className="bg-black text-red-500">
+          HAHAHAHA
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
