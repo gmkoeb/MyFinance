79e99fdc8074fa0a8bb93a2f418bab8f8732ceb1
Gabriel Manika <gmkoeb@gmail.com>
2024-12-01 11:39:38 -0300

    Change API base url to match the deployed url

--- web/api/axios.tsx
+++ web/api/axios.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import Cookies from 'js-cookie'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000'
+  baseURL: 'https://finance-api.bosswatchtower.com'
 })
 
 if (Cookies.get('token')) {
